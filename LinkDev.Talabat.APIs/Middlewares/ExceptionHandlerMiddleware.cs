8c4d2499de88751b931e9f44607c9757c094f002
AhmeddAymannHalim <153849097+AhmeddAymannHalim@users.noreply.github.com>
2024-10-24 18:36:36 +0300

    Part 06 Auth Service - Login and Register

--- LinkDev.Talabat.APIs/Middlewares/ExceptionHandlerMiddleware.cs
+++ LinkDev.Talabat.APIs/Middlewares/ExceptionHandlerMiddleware.cs
@@ -63,6 +63,14 @@ namespace LinkDev.Talabat.APIs.Middlewares
 
                     await httpContext.Response.WriteAsync(response.ToString());
                     break;
+                case ValidationException validationException :
+                    httpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    httpContext.Response.ContentType = "application/json";
+
+                    response = new ApiResponse(400,ex.Message);
+
+                    await httpContext.Response.WriteAsync(response.ToString());
+                    break;
 
                 case BadRequestException:
                     httpContext.Response.StatusCode = (int) HttpStatusCode.BadRequest;
@@ -75,7 +83,7 @@ namespace LinkDev.Talabat.APIs.Middlewares
                 case UnAuthorizedException:
                     httpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
                     httpContext.Response.ContentType = "application/json";
-                    response = new ApiResponse(401, "Invalid Email Or Password ! Please Try agian ...");
+                    response = new ApiResponse(401, "Invalid Email Or Password! Please Try agian ...");
 
                     await httpContext.Response.WriteAsync(response.ToString());
                     break;

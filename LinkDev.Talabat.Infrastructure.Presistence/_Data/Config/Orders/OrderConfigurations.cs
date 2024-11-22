ef408047f6f503bfa5db6cc0a3911cecdcbb3e11
AhmeddAymannHalim <153849097+AhmeddAymannHalim@users.noreply.github.com>
2024-11-22 14:21:10 +0200

    Part 02 Order Module - Configurartion Classes

---
+++ LinkDev.Talabat.Infrastructure.Presistence/_Data/Config/Orders/OrderConfigurations.cs
@@ -0,0 +1,42 @@
+﻿using LinkDev.Talabat.Core.Domain.Entities.Orders;
+using LinkDev.Talabat.Infrastructure.Presistence._Data.Config.Base;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Infrastructure.Presistence._Data.Config.Orders
+{
+    internal class OrderConfigurations : BaseAuditableEntityConfigurations<Order,int>
+    {
+        public override void Configure(EntityTypeBuilder<Order> builder)
+        {
+            base.Configure(builder);
+
+            builder.OwnsOne(O => O.ShippingAddress, shippingAddress => shippingAddress.WithOwner());
+
+            builder.Property(O => O.Status)
+                       .HasConversion
+                       (
+
+                            (OStatus) => OStatus.ToString(),
+                            (OStatus) => (OrderStatus)Enum.Parse(typeof(OrderStatus), OStatus));
+
+            builder.Property(O => O.SubTotal)
+                .HasColumnType("decimal(8, 2)");
+
+            builder.HasOne(O => O.DeliveryMethod)
+                .WithMany()
+                .HasForeignKey(O => O.DeliveryMethodId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.HasMany(O => O.OrderItems)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+                
+                
+        }
+    }
+}

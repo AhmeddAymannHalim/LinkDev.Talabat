2ae05ec29ca83597b0ca501b30442998b1642f59
AhmeddAymannHalim <153849097+AhmeddAymannHalim@users.noreply.github.com>
2024-11-22 19:00:40 +0200

    Part 07 CreateOrder endpoint

--- LinkDev.Talabat.Infrastructure.Presistence/DependencyInjection.cs
+++ LinkDev.Talabat.Infrastructure.Presistence/DependencyInjection.cs
@@ -29,7 +29,7 @@ namespace LinkDev.Talabat.Infrastructure.Presistence
             // services.AddScoped<IStoreContextInitializer, StoreContextInitializer>();
             services.AddScoped(typeof(IStoreDbInitializer), typeof(StoreDbInitializer));
 
-            services.AddScoped(typeof(ISaveChangesInterceptor), typeof(BaseAuditableEntityInterceptor));
+            //services.AddScoped(typeof(ISaveChangesInterceptor), typeof(BaseAuditableEntityInterceptor));
 
             #endregion
 

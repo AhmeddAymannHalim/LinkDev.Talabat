ed3f57b0cb5d606bbfa3a2adb2ddb85b68062486
AhmeddAymannHalim <153849097+AhmeddAymannHalim@users.noreply.github.com>
2024-11-23 20:57:23 +0200

    Part 10 Get Current User

--- LinkDev.Talabat.Core.Application/Services/Auth/AuthService.cs
+++ LinkDev.Talabat.Core.Application/Services/Auth/AuthService.cs
@@ -19,6 +19,21 @@ namespace LinkDev.Talabat.Core.Application.Services.Auth
     {
         private readonly JwtSettings _jwtSettings = jwtSettings.Value;
 
+        public async Task<UserDto> GetCurrentUser(ClaimsPrincipal claimsPrincipal)
+        {
+
+            var email = claimsPrincipal.FindFirstValue(ClaimTypes.Email);
+            var user = await userManager.FindByEmailAsync(email!);
+
+            return new UserDto()
+            {
+                Email = user!.Email!,
+                DisplayName = user.DisplayName,
+                Id = user.Id,
+                Token = await GenerateTokenAsync(user),
+            };
+        }
+
         public async Task<UserDto> LoginAsync(LoginDto model)
         {                    
             var user = await userManager.FindByEmailAsync(model.Email);

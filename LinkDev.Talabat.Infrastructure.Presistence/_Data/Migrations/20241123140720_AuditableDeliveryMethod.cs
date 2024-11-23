29149af5c0ed2776b697172bda05e2a80ea90f44
AhmeddAymannHalim <153849097+AhmeddAymannHalim@users.noreply.github.com>
2024-11-23 17:11:45 +0200

    Part 08 GetOrdersForUser, GetOrderForUser, GetDeliveryMethods endpoints

---
+++ LinkDev.Talabat.Infrastructure.Presistence/_Data/Migrations/20241123140720_AuditableDeliveryMethod.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinkDev.Talabat.Infrastructure.Presistence.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuditableDeliveryMethod : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "DelivryMethods",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "DelivryMethods",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastModifiedBy",
+                table: "DelivryMethods",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedOn",
+                table: "DelivryMethods",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "DelivryMethods");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "DelivryMethods");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                table: "DelivryMethods");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedOn",
+                table: "DelivryMethods");
+        }
+    }
+}
